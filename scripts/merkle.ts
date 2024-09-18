d7838d45c33e5dcee5b7a0f11fc0d2ba40fe6718
frankie-powers <franklin.power21@gmaul.com>
2024-09-18 17:48:42 +0100

    updated merkle.ts

--- scripts/merkle.ts
+++ scripts/merkle.ts
@@ -3,23 +3,24 @@ import fs from "fs";
 import csv from "csv-parser";
 
 interface AirdropEntry {
-    user_address: string;  // Ethereum address
-    amount: number;  // Token amount eligible for airdrop
+  address: string; // Ethereum addresses
+  amount: number; // Token amount eligible for airdrop
 }
 
-const values: [string, number][] = [];  // Array to store values from CSV
+const values: [string, number][] = []; // Array to store values from CSV
 
-const feedFile = "feed-files/addresses.csv";
+const treeFile = "merkletree-files/addresses.csv"; // Path to my CSV file
 
 // Read the CSV file and populate the values array
-fs.createReadStream(feedFile)
+fs.createReadStream(treeFile)
   .pipe(csv())
   .on("data", (row: AirdropEntry) => {
-    values.push([row.user_address, row.amount]);
+    values.push([row.address, row.amount]);
   })
   .on("end", () => {
     // Create a Merkle tree from the values
     const tree = StandardMerkleTree.of(values, ["address", "uint256"]);
+
     console.log("Merkle Root:", tree.root);
 
     // Write the tree to a JSON file
@@ -27,10 +28,11 @@ fs.createReadStream(feedFile)
 
     // Load the tree from the JSON file
     try {
-        
-      const loadedTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("tree.json", "utf8")));
+      const loadedTree = StandardMerkleTree.load(
+        JSON.parse(fs.readFileSync("tree.json", "utf8"))
+      );
       const proofs: any = {};
-      
+
       // Iterate over the entries in the loaded tree
       for (const [i, v] of loadedTree.entries()) {
         // Get the proof for each address
@@ -38,30 +40,22 @@ fs.createReadStream(feedFile)
         proofs[v[0]] = proof; // Store the proof with the address as the key
 
         // Check for a specific address and get the proof if found
-        if (v[0] === '0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2') {
+        if (v[0] === "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2") {
           const proof = loadedTree.getProof(i);
-          console.log('Proof:', proof);
+          console.log("Proof:", proof);
         }
 
         // Write all proofs to a JSON file
-        fs.writeFileSync("feed-files/proofs.json", JSON.stringify(proofs, null, 2));
+        fs.writeFileSync(
+          "merkletree-files/proofs.json",
+          JSON.stringify(proofs, null, 2)
+        );
         console.log("All proofs have been saved to 'proofs.json'.");
-
       }
     } catch (err) {
       console.error("Error reading or processing 'tree.json':", err);
     }
   })
   .on("error", (err: Error) => {
-    console.error(`Error reading ${feedFile}:`, err);
+    console.error(`Error reading ${treeFile}:`, err);
   });
-
-
-
-
-
-
-
-
-
-  
